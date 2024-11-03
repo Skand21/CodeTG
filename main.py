b77e5b4776368f90b1ffb7326e9dc6883c13650b
Артём <68154802+Skand21@users.noreply.github.com>
2024-11-03 16:11:14 +0300

    Update Btns and Test

--- main.py
+++ main.py
@@ -1,13 +1,80 @@
 import telebot
+from telebot import types
 
 botTimeWeb = telebot.TeleBot('7463534277:AAHZ29LmrJIwzFTPmJ5h-s1UzmjJ3Brzoi4')
 
-from telebot import types
+
+# Список вопросов и ответов
+correctotveti = 0
+questions = [
+    {"text": "Что такое переменная в C++?", "correct": "true", "answers": [
+        {"text": "Переменная - это место в памяти, где хранится значение", "callback_data": "true"},
+        {"text": "Переменная - это название программы", "callback_data": "false"},
+        {"text": "Переменная используется только для ввода данных", "callback_data": "false"},
+        {"text": "Переменная - это то же самое, что и функция", "callback_data": "false"},
+    ]},
+    {"text": "Какой тип данных используется для хранения целых чисел в C++?", "correct": "true", "answers": [
+        {"text": "int", "callback_data": "true"},
+        {"text": "char", "callback_data": "false"},
+        {"text": "float", "callback_data": "false"},
+        {"text": "bool", "callback_data": "false"},
+    ]},
+    {"text": "Что означает оператор '==' в C++?", "correct": "true", "answers": [
+        {"text": "Присвоение значения", "callback_data": "false"},
+        {"text": "Сравнение на равенство", "callback_data": "true"},
+        {"text": "Логическое И", "callback_data": "false"},
+        {"text": "Увеличение значения", "callback_data": "false"},
+    ]},
+    {"text": "Какой из следующих циклов используется для выполнения кода определённое количество раз?", "correct": "true", "answers": [
+        {"text": "for", "callback_data": "true"},
+        {"text": "while", "callback_data": "false"},
+        {"text": "if", "callback_data": "false"},
+        {"text": "switch", "callback_data": "false"},
+    ]},
+    {"text": "Какой символ используется для завершения строки кода в C++?", "correct": "true", "answers": [
+        {"text": ":", "callback_data": "false"},
+        {"text": ";", "callback_data": "true"},
+        {"text": ".", "callback_data": "false"},
+        {"text": ",", "callback_data": "false"},
+    ]},
+    {"text": "Как создать функцию в C++?", "correct": "true", "answers": [
+        {"text": "void функция()", "callback_data": "true"},
+        {"text": "function функция()", "callback_data": "false"},
+        {"text": "create функция()", "callback_data": "false"},
+        {"text": "func функция()", "callback_data": "false"},
+    ]},
+    {"text": "Какой оператор используется для вывода текста на экран?", "correct": "true", "answers": [
+        {"text": "cout", "callback_data": "true"},
+        {"text": "cin", "callback_data": "false"},
+        {"text": "printf", "callback_data": "false"},
+        {"text": "write", "callback_data": "false"},
+    ]},
+    {"text": "Какой тип данных может хранить только два значения: true или false?", "correct": "true", "answers": [
+        {"text": "bool", "callback_data": "true"},
+        {"text": "int", "callback_data": "false"},
+        {"text": "string", "callback_data": "false"},
+        {"text": "double", "callback_data": "false"},
+    ]},
+    {"text": "Какой оператор используется для увеличения значения переменной на 1?", "correct": "true", "answers": [
+        {"text": "++", "callback_data": "true"},
+        {"text": "--", "callback_data": "false"},
+        {"text": "+=", "callback_data": "false"},
+        {"text": "*=", "callback_data": "false"},
+    ]},
+    {"text": "Что делает функция main() в C++?", "correct": "true", "answers": [
+        {"text": "Запускает выполнение программы", "callback_data": "true"},
+        {"text": "Завершает выполнение программы", "callback_data": "false"},
+        {"text": "Выводит текст на экран", "callback_data": "false"},
+        {"text": "Создает переменную", "callback_data": "false"},
+    ]}
+]
+# Переменная для отслеживания текущего вопроса
+current_question_index = {}
 
 # Обработка команды /start
 @botTimeWeb.message_handler(commands=['start'])
 def startBot(message):
-    first_mess = f"Привет, <b>{message.from_user.first_name}</b>!\nЯ бот, который помогает с изучением языка С++ и отслеживает твой прогресс. Выберите с чего хотите начать"
+    first_mess = f"Привет, <b>{message.from_user.first_name}</b>!\nЯ бот, который помогает с изучением языка С++ и отслеживает твой прогресс. Выберите, с чего хотите начать."
     markup = types.InlineKeyboardMarkup()
     button_hochutest = types.InlineKeyboardButton(text='Узнать свой уровень', callback_data='test')
     button_nehochutest = types.InlineKeyboardButton(text='Обучиться с нуля', callback_data='nol')
@@ -16,38 +83,50 @@ def startBot(message):
     markup.add(button_nehochutest)
     botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
 
-#1
-
-# Обработка нажатия на кнопки "Учитель" и "Ученик"
+# Обработка нажатия на кнопки "Узнать уровень" и "Обучиться с нуля"
 @botTimeWeb.callback_query_handler(func=lambda call: True)
 def handle_query(call):
     if call.data == 'test':
-        # Если выбрано "Учитель", просим ввести код
-        botTimeWeb.send_message(call.message.chat.id, ".")
-        botTimeWeb.register_next_step_handler(call.message, get_teacher_code)
-        for question in range(1,11):
-            text = f"Вопрос {question}"
-            markup = types.InlineKeyboardMarkup()
-            button_hochutest = types.InlineKeyboardButton(text='Узнать свой уровень', callback_data='test')
-            button_nehochutest = types.InlineKeyboardButton(text='Обучиться с нуля', callback_data='nol')
-
-            markup.add(button_hochutest)
-            markup.add(button_nehochutest)
-            botTimeWeb.send_message(call.message.chat.id, text, parse_mode='html', reply_markup=markup)
+        current_question_index[call.from_user.id] = 0  # Начинаем с первого вопроса
+        ask_question(call.message.chat.id, call.from_user.id)
     elif call.data == 'nol':
-        # Если выбран "Ученик", просим ввести класс
-        botTimeWeb.send_message(call.message.chat.id, "")
+        botTimeWeb.send_message(call.message.chat.id, "Отлично, начинаем с нуля!")
         botTimeWeb.register_next_step_handler(call.message, get_student_class)
+    elif call.data in ['true', 'false']:
+        check_answer(call)
 
-# Обработка ввода кода учителя
-def get_teacher_code(message):
-    teacher_code = message.text
-    # Здесь вы можете добавить логику для проверки кода учителя
-    if teacher_code == "1234":  # Пример кода
-        botTimeWeb.send_message(message.chat.id, "Код подтвержден! Добро пожаловать, учитель.")
+# Функция для задания вопроса
+def ask_question(chat_id, user_id):
+    index = current_question_index.get(user_id, 0)
+    if index < len(questions):
+        question = questions[index]
+        text = question["text"]
+        markup = types.InlineKeyboardMarkup()
+
+        # Добавляем ответы как кнопки
+        for answer in question["answers"]:
+            markup.add(types.InlineKeyboardButton(text=answer["text"], callback_data=answer["callback_data"]))
+
+        botTimeWeb.send_message(chat_id, text, parse_mode='html', reply_markup=markup)
     else:
-        botTimeWeb.send_message(message.chat.id, "Неверный код. Попробуйте еще раз.")
-        botTimeWeb.register_next_step_handler(message, get_teacher_code)
+        botTimeWeb.send_message(chat_id, f"Тест завершен! Ваш результат: {correctotveti} / 10")
+
+# Функция для проверки ответа
+def check_answer(call):
+    global correctotveti
+    user_id = call.from_user.id
+    index = current_question_index.get(user_id, 0)
+    correct_answer = questions[index]["correct"]
+
+    if call.data == correct_answer:
+        botTimeWeb.send_message(call.message.chat.id, "Правильный ответ!")
+        correctotveti += 1
+    else:
+        botTimeWeb.send_message(call.message.chat.id, "Неправильный ответ.")
+
+    # Переход к следующему вопросу
+    current_question_index[user_id] += 1
+    ask_question(call.message.chat.id, user_id)
 
 # Обработка ввода класса ученика
 def get_student_class(message):
